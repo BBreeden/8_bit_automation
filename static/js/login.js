94a01bebd5035d8091474a9691ada831efcb5c82
Brian Breeden <breedenb@vt.edu>
2021-03-04 15:35:01 -0500

    refactoring DOM variables

--- static/js/login.js
+++ static/js/login.js
@@ -1,7 +1,4 @@
 function login(){
-    login_field = document.getElementById('login_field');
-    password_field = document.getElementById('password_field');
-
     if ((login_field.value.toLowerCase() == 'admin') && (password_field.value.toLowerCase() =='kirby')){
         login_field.classList.remove('is-error');
         password_field.classList.remove('is-error');
@@ -16,14 +13,22 @@ function login(){
 }
 
 function cancel(){
-    login_field = document.getElementById('login_field');
-    password_field = document.getElementById('password_field');
-    
     login_field.value="";
     password_field.value="";
+
+    removeStatusCSS();
+}
+
+function removeStatusCSS(){
+    login_field.classList.remove('is-error');
+    password_field.classList.remove('is-error');
+    login_field.classList.remove('is-success');
+    password_field.classList.remove('is-success');
 }
 
 window.onload = function() {
     document.getElementById('login_btn').addEventListener('click', login);
     document.getElementById('cancel_btn').addEventListener('click', cancel);
+    login_field = document.getElementById('login_field');
+    password_field = document.getElementById('password_field');
   };
