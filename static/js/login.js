dc4ea84fc6f2d380f80aa889de0d3ab6e3084e59
Brian Breeden <breedenb@vt.edu>
2021-03-04 19:02:48 -0500

    added alert messages.

--- static/js/login.js
+++ static/js/login.js
@@ -9,6 +9,13 @@ function login(){
         password_field.classList.remove('is-success');
         login_field.classList.add('is-error');
         password_field.classList.add('is-error');
+        login_fail_msg.classList.remove('hidden');
+        login_counter++;
+
+        if (login_counter == 3) {
+            forgot_password.classList.remove('hidden');
+            secret_sound.play()
+        }
     }
 }
 
@@ -24,6 +31,9 @@ function removeStatusCSS(){
     password_field.classList.remove('is-error');
     login_field.classList.remove('is-success');
     password_field.classList.remove('is-success');
+    forgot_password.classList.add('hidden');
+    login_fail_msg.classList.add('hidden');
+    login_counter = 0;
 }
 
 window.onload = function() {
@@ -31,4 +41,8 @@ window.onload = function() {
     document.getElementById('cancel_btn').addEventListener('click', cancel);
     login_field = document.getElementById('login_field');
     password_field = document.getElementById('password_field');
+    login_fail_msg = document.getElementById('login-fail-alert');
+    forgot_password = document.getElementById('login-forgot');
+    login_counter = 0;
+    secret_sound = new Audio('../static/misc/secret.mp3')
   };
