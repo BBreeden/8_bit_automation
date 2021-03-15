58efbc33cf590e88129c633c3532a756fc359e45
Brian Breeden <breedenb@vt.edu>
2021-03-15 17:27:52 -0400

    added a placeholder form.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from forms import AppForm
+import os
 
 app = Flask(__name__)
+
+# CSRF Protection
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 app.config["DEBUG"] = True
 
@@ -12,3 +18,11 @@ def index():
 def home():
     return render_template("home.html")
 
+@app.route('/forms')
+def forms():
+    form = AppForm()
+    return render_template("form.html", form = form)
+
+@app.route('/confirmation')
+def confirmation():
+    return render_template("confirmation.html")
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SubmitField, TextAreaField, validators
+from wtforms.validators import DataRequired, Length, Email
+import email_validator
+
+class AppForm(FlaskForm):
+    name = StringField('Name', [ DataRequired() ])
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
+    body = TextAreaField('Message', [ DataRequired(), Length(min=20, message=('Your message is not quite long enough, try again.')) ])
+    submit = SubmitField('Send')
