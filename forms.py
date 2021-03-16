dc9c0f7885fd0fb5b72dfe20c902fd8a7e834759
Brian Breeden <breedenb@vt.edu>
2021-03-16 17:28:41 -0400

    added more form fields.

--- forms.py
+++ forms.py
@@ -1,10 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField, TextAreaField, validators
+from wtforms import StringField, TextField, SubmitField, TextAreaField, validators, IntegerField, RadioField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 import email_validator
 
 class AppForm(FlaskForm):
-    name = StringField('Name', [ DataRequired() ])
+    fname = StringField('First Name', [ DataRequired() ])
+    lname = StringField('Last Name', [ DataRequired() ])
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
-    body = TextAreaField('Message', [ DataRequired(), Length(min=20, message=('Your message is not quite long enough, try again.')) ])
+    street = StringField('Street Address', [ DataRequired() ])
+    city = StringField('City', [DataRequired()])
+    zcode = IntegerField('Zip Code', [DataRequired(), Length(min=5, max=5, message=('Please enter a valid, 5 digit zip code.')) ])
+    experience = TextField('Experience', [DataRequired()])
+    salary = StringField('Requested Salary', [DataRequired()])
+    hours = RadioField('Are you willing to work 90 hour weeks?', choices=[('Yes','yes'),('Also Yes','yes2')])
+    boss = BooleanField('It is rumored that the supervisor of this position is a 8 foot tall, firebreathing dragon turtle thing. Are you cool with that?', [DataRequired()])
+    mario = BooleanField('If an Italian plumber shows up and starts making a mess, do you agree to clean it up?', [DataRequired()])
     submit = SubmitField('Send')
