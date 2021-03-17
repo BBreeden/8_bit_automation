29d02492041e15f39d2622a5b3d6b325872f9760
Brian Breeden <breedenb@vt.edu>
2021-03-17 18:39:28 -0400

    form work

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ class AppForm(FlaskForm):
     zcode = IntegerField('Zip Code', [DataRequired(), Length(min=5, max=5, message=('Please enter a valid, 5 digit zip code.')) ])
     experience = TextField('Experience', [DataRequired()])
     salary = StringField('Requested Salary', [DataRequired()])
-    hours = RadioField('Are you willing to work 90 hour weeks?', choices=[('Yes','yes'),('Also Yes','yes2')])
+    work = RadioField('Are you willing to work 90 hour weeks?', [DataRequired()], choices=[('Yes','yes'),('Also Yes','yes2')])
     boss = BooleanField('It is rumored that the supervisor of this position is a 8 foot tall, firebreathing dragon turtle thing. Are you cool with that?', [DataRequired()])
     mario = BooleanField('If an Italian plumber shows up and starts making a mess, do you agree to clean it up?', [DataRequired()])
     submit = SubmitField('Send')
